870b29633f86b0d1fd75b19b5ef412392f388e2f
ahmedcodeps <ai7138572@gmail.com>
2026-08-16 16:28:32 +0300

    Delete QA.cs

--- QA.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-
-[GlobalClass]
-public partial class QA : Resource
-{
-    [Export] public string Question { get; set; } = "";
-    [Export] public string Answer { get; set; } = "";
-}
